7030237b9cbae96635c8fc1a38c24420b7fc9170
Eugene Kostenko <eugenek@netmasterclass.net>
2019-10-24 00:40:13 +0700

    bump versions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pssh "0.1"
   :description "Выполняет команду параллельно на нескольких терминалах"
-  :dependencies [[org.clojure/clojure "1.4.0-beta5"]
-                 [org.clojure/tools.cli "0.2.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.cli "0.4.2"]
                  [org.clojars.rosejn/clansi "1.2.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
   :main pssh.core)
